4a08324d86ed68035871524623cabdf037b4dbc9
Prabu <prabu.undefined@gmail.com>
2022-10-03 16:51:37 +0530

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      borderWidth: {
+        1: "1px",
+      },
+      colors: {
+        darkGray: "#191919",
+        navyBlue: "#2D4263",
+        kindaRed: "#C84B31",
+        beige: "#ECDBBA",
+      },
+    },
   },
   plugins: [],
 };
