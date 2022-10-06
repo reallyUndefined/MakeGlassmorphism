c6a2f7e03ef85a5c89d272aaeb1534ffc27c0f34
Prabu <prabu.undefined@gmail.com>
2022-10-06 10:47:59 +0530

    logo and favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import Options from "./components/Options";
 import Output from "./components/Output";
 import Preview from "./components/Preview";
+import logo from "./images/logo.png";
 
 function App() {
   return (
     <div className="min-h-screen bg-darkGray text-beige">
       <div className="container px-4 py-8 mx-auto">
+        <div className="mb-4">
+          <img src={logo} alt="" className="mx-auto w-32 rounded-md lg:w-44" />
+        </div>
         <Options />
         <div className="grid gap-4 mt-4 md:grid-cols-2 items-start">
           <Preview />
